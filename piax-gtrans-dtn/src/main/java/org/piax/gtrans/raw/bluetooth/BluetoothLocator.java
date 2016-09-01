12ceb0b2a55a87c63f7a7cd22aa116d2204240f8
teranisi <teranisi@gohome.org>
2016-09-01 16:53:58 +0900

    Updated copyright.

--- piax-gtrans-dtn/src/main/java/org/piax/gtrans/raw/bluetooth/BluetoothLocator.java
+++ piax-gtrans-dtn/src/main/java/org/piax/gtrans/raw/bluetooth/BluetoothLocator.java
@@ -3,6 +3,7 @@
  * 
  * Copyright (c) 2012-2015 National Institute of Information and 
  * Communications Technology
+ * Copyright (c) 2016 PIAX development team
  * 
  * You can redistribute it and/or modify it under either the terms of
  * the AGPLv3 or PIAX binary code license. See the file COPYING
