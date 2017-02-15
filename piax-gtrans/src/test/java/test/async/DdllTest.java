2025b9a0966a1b3b79927ab36612d6ddfe59fe32
Kota Abe <k-abe@media.osaka-cu.ac.jp>
2017-02-15 12:56:47 +0900

    add JUnit test

---
+++ piax-gtrans/src/test/java/test/async/DdllTest.java
@@ -0,0 +1,242 @@
+package test.async;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Test;
+import org.piax.common.PeerId;
+import org.piax.common.PeerLocator;
+import org.piax.common.TransportId;
+import org.piax.gtrans.ChannelTransport;
+import org.piax.gtrans.IdConflictException;
+import org.piax.gtrans.Peer;
+import org.piax.gtrans.async.EventExecutor;
+import org.piax.gtrans.async.LocalNode;
+import org.piax.gtrans.async.NetworkParams;
+import org.piax.gtrans.async.NodeFactory;
+import org.piax.gtrans.async.Sim;
+import org.piax.gtrans.ov.ddll.DdllKey;
+import org.piax.gtrans.ov.ddllasync.DdllStrategy;
+import org.piax.gtrans.ov.ddllasync.DdllStrategy.DdllNodeFactory;
+import org.piax.gtrans.ov.ddllasync.DdllStrategy.SetRNakMode;
+import org.piax.gtrans.raw.emu.EmuLocator;
+import org.piax.gtrans.raw.tcp.TcpLocator;
+import org.piax.gtrans.raw.udp.UdpLocator;
+import org.piax.util.UniqId;
+
+public class DdllTest {
+    public static class Indirect<U> {
+        U obj;
+    }
+
+    static LocalNode[] nodes;
+    static boolean REALTIME = false;
+
+    private static LocalNode createNode(NodeFactory factory, int key) {
+        return createNode(factory, key, NetworkParams.HALFWAY_DELAY);
+    }
+
+    private static LocalNode createNode(NodeFactory factory, int key, int latency) {
+        TransportId transId = new TransportId("SimTrans");
+        if (REALTIME) {
+            Peer peer = Peer.getInstance(new PeerId("P" + key));
+            DdllKey k = new DdllKey(key, new UniqId(peer.getPeerId()), "", null);
+            PeerLocator loc = newLocator("emu", key);
+            ChannelTransport<?> trans;
+            try {
+                trans = peer.newBaseChannelTransport(loc);
+                return factory.createNode(transId, trans, k, latency);
+            } catch (IOException | IdConflictException e) {
+                throw new Error("something wrong!", e);
+            }
+        } else {
+            UniqId p = new UniqId("P");
+            DdllKey k = new DdllKey(key, p, "", null);
+            try {
+                return factory.createNode(null, null, k, latency);
+            } catch (IOException | IdConflictException e) {
+                throw new Error("something wrong!", e);
+            }
+        }
+    }
+
+    static PeerLocator newLocator(String locatorType, int vport) {
+        PeerLocator peerLocator;
+        if (locatorType.equals("emu")) {
+            peerLocator = new EmuLocator(vport);
+        } else if (locatorType.equals("udp")) {
+            peerLocator = new UdpLocator(
+                    new InetSocketAddress("localhost", 10000 + vport));
+        } else {
+            peerLocator = new TcpLocator(
+                    new InetSocketAddress("localhost", 10000 + vport));
+        }
+        return peerLocator;
+    }
+    
+    public static void init() {
+        Sim.verbose = true;
+        if (REALTIME) {
+            EventExecutor.realtime.set(true);
+        }
+        DdllStrategy.pingPeriod.set(10000);
+        DdllStrategy.setrnakmode.set(SetRNakMode.SETRNAK_OPT2);
+    }
+    
+    static LocalNode[] createNodes(int num) {
+        nodes = new LocalNode[num];
+        for (int i = 0; i < num; i++) {
+            nodes[i] = createNode(new DdllNodeFactory(), i * 100, 100);
+        }
+        return nodes;
+    }
+    
+    public static void dump(LocalNode[] nodes) {
+        System.out.println("node dump:");
+        for (int i = 0; i < nodes.length; i++) {
+            System.out.println(i + ": " + nodes[i].toStringDetail()
+                    + ", latency=" + nodes[i].latency);
+        }
+    }
+
+    @SafeVarargs
+    final void checkCompleted(CompletableFuture<Boolean>... futures) {
+        for (int i = 0; i < futures.length; i++) {
+            CompletableFuture<Boolean> f = futures[i];
+            assertTrue(f.isDone());
+            try {
+                assertTrue(f.get());
+            } catch (InterruptedException | ExecutionException e) {
+                fail(e.toString());
+            }
+        }
+    }
+
+    void checkConsistent(LocalNode... nodes) {
+        int s = nodes.length;
+        for (int i = 0; i < s ; i++) {
+            assertTrue(nodes[i].succ == nodes[(i + 1) % s]);
+            assertTrue(nodes[i].pred == nodes[(i - 1 + s) % s]);
+        }
+    }
+    
+    @Test
+    public void testBasicInsDel() {
+        System.out.println("** testBasicInsDel");
+        init();
+        nodes = createNodes(2);
+        nodes[0].joinInitialNode();
+        {
+            CompletableFuture<Boolean> f = nodes[1].joinAsync(nodes[0]);
+            EventExecutor.startSimulation(30000);
+            checkCompleted(f);
+        }
+        checkConsistent(nodes);
+        {
+            CompletableFuture<Boolean> f = nodes[1].leaveAsync();
+            EventExecutor.startSimulation(30000);
+            checkCompleted(f);
+            checkConsistent(nodes[0]);
+        }
+
+        {
+            CompletableFuture<Boolean> f = nodes[0].leaveAsync();
+            EventExecutor.startSimulation(30000);
+            checkCompleted(f);
+        }
+    }
+
+    @Test
+    public void testParallelInsDel() {
+        System.out.println("** testParallelInsDel");
+        init();
+        nodes = createNodes(4);
+        nodes[0].joinInitialNode();
+        {
+            CompletableFuture<Boolean> f1 = nodes[1].joinAsync(nodes[0]);
+            CompletableFuture<Boolean> f2 = nodes[2].joinAsync(nodes[0]);
+            CompletableFuture<Boolean> f3 = nodes[3].joinAsync(nodes[0]);
+            EventExecutor.startSimulation(30000);
+            checkCompleted(f1, f2, f3);
+            checkConsistent(nodes);
+        }
+        
+        {
+            CompletableFuture<Boolean> f1 = nodes[1].leaveAsync();
+            CompletableFuture<Boolean> f2 = nodes[2].leaveAsync();
+            CompletableFuture<Boolean> f3 = nodes[3].leaveAsync();
+            EventExecutor.startSimulation(30000);
+            checkCompleted(f1, f2, f3);
+            checkConsistent(nodes[0]);
+        }
+    }
+
+    @Test
+    public void testFix() {
+        System.out.println("** testFix");
+        init();
+        nodes = createNodes(4);
+        nodes[0].joinInitialNode();
+        {
+            CompletableFuture<Boolean> f1 = nodes[1].joinAsync(nodes[0]);
+            CompletableFuture<Boolean> f2 = nodes[2].joinAsync(nodes[0]);
+            CompletableFuture<Boolean> f3 = nodes[3].joinAsync(nodes[0]);
+            EventExecutor.sched(10000, () -> {
+                nodes[1].fail();
+                nodes[2].fail();  
+            });
+            EventExecutor.startSimulation(30000);
+            checkCompleted(f1, f2, f3);
+            dump(nodes);
+            checkConsistent(nodes[0], nodes[3]);
+        }
+    }
+
+    @Test
+    public void testFix2() {
+        System.out.println("** testFix2");
+        init();
+        nodes = createNodes(3);
+        nodes[0].joinInitialNode();
+        {
+            CompletableFuture<Boolean> f1 = nodes[1].joinAsync(nodes[0]);
+            CompletableFuture<Boolean> f2 = nodes[2].joinAsync(nodes[0]);
+            Indirect<CompletableFuture<Boolean>> f3 = new Indirect<>();
+            EventExecutor.sched(10000, () -> {
+                nodes[1].fail();
+                f3.obj = nodes[2].leaveAsync();
+            });
+            EventExecutor.startSimulation(30000);
+            assertNotNull(f3.obj);
+            checkCompleted(f1, f2, f3.obj);
+            dump(nodes);
+            checkConsistent(nodes[0]);
+        }
+    }
+
+    @Test
+    public void testFix3() {
+        System.out.println("** testFix3");
+        init();
+        nodes = createNodes(2);
+        nodes[0].joinInitialNode();
+        {
+            CompletableFuture<Boolean> f1 = nodes[1].joinAsync(nodes[0]);
+            Indirect<CompletableFuture<Boolean>> f2 = new Indirect<>();
+            EventExecutor.sched(10000, () -> {
+                nodes[0].fail();
+                f2.obj = nodes[1].leaveAsync();
+            });
+            EventExecutor.startSimulation(30000);
+            assertNotNull(f2.obj);
+            dump(nodes);
+            checkCompleted(f1, f2.obj);
+        }
+    }
+}
