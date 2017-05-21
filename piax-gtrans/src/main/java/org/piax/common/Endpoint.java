1afce93415221871b1724957466fc3b61fa70ec8
teranisi <teranisi@gohome.org>
2017-05-21 13:01:41 +0900

    Added a newEndpoint API.

--- piax-gtrans/src/main/java/org/piax/common/Endpoint.java
+++ piax-gtrans/src/main/java/org/piax/common/Endpoint.java
@@ -13,9 +13,29 @@
 
 package org.piax.common;
 
+import org.piax.gtrans.ProtocolUnsupportedException;
+import org.piax.gtrans.netty.NettyEndpoint;
+import org.piax.gtrans.raw.tcp.TcpLocator;
+import org.piax.gtrans.raw.udp.UdpLocator;
+
 /**
  * A class that corresponds to an endpoint.
  */
 public interface Endpoint extends Key {
-
+    public static Endpoint newEndpoint(String spec) throws ProtocolUnsupportedException {
+        String specs[] = spec.split(":");
+        if (specs[0].equals("-tcp")) {
+            return new TcpLocator(spec);
+        }
+        else if (specs[0].equals("-udp")) {
+            return new UdpLocator(spec);
+        }
+        else {
+            return NettyEndpoint.newEndpoint(spec);
+        }
+    }
+    public static void main(String args[]) throws Exception {
+        Endpoint ep = Endpoint.newEndpoint("id:-1.0:tcp:localhost:12367");
+        System.out.println(ep);
+    }
 }
