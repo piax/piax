dffb0dfee4a066c28679b3ba3cce3b783c157369
teranisi <teranisi@gohome.org>
2017-05-21 15:42:59 +0900

    Changes for performance improvement.

--- piax-gtrans/src/main/java/org/piax/gtrans/ov/ddll/DdllKey.java
+++ piax-gtrans/src/main/java/org/piax/gtrans/ov/ddll/DdllKey.java
@@ -95,19 +95,19 @@ public class DdllKey implements Comparable<DdllKey>, Serializable, Cloneable {
         int cmp = keyComp.compare(rawKey, o.rawKey);
         if (cmp != 0) {
             if (logger.isDebugEnabled()) {
-                logger.debug("compareTo: " + this + ", " + o + " = " + cmp);
+                logger.debug("compareTo: {}, {} = {}", this, o, cmp);
             }
             return cmp;
         }
         cmp = uniqId.compareTo(o.uniqId);
         if (cmp != 0) {
             if (logger.isDebugEnabled()) {
-                logger.debug("compareTo: " + this + ", " + o + " = " + cmp);
+                logger.debug("compareTo: {}, {} = {}", this, o, cmp);
             }
             return cmp;
         }
         if (logger.isDebugEnabled()) {
-            logger.debug("compareTo: " + this + ", " + o + " = 0");
+            logger.debug("compareTo: {}, {} = 0", this, o);
         }
         return 0;
     }
