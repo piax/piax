42f0025754b94d75f1fab085d30bd271c640b665
mrt <mrt@acutus.co.jp>
2018-02-20 13:46:19 +0900

    Reindent TestSuzakuKeyAsync.

--- piax-gtrans-suzaku/src/test/java/org/piax/gtrans/ov/suzaku/TestSuzakuKeyAsync.java
+++ piax-gtrans-suzaku/src/test/java/org/piax/gtrans/ov/suzaku/TestSuzakuKeyAsync.java
@@ -39,31 +39,32 @@ import org.slf4j.LoggerFactory;
 
 class TestSuzakuKeyAsync {
     private static final Logger logger = LoggerFactory.getLogger(TestSuzakuKeyAsync.class);
-	CompletableFuture<Boolean> f = null;
-	boolean result;
-	List<String> results;
+    CompletableFuture<Boolean> f = null;
+    boolean result;
+    List<String> results;
+
     @Test
     public void addKeyInTimer() throws Exception {
         Suzaku<Destination, StringKey> s1 = new Suzaku<>("tcp:localhost:12367");
         Suzaku<Destination, StringKey> s2 = new Suzaku<>("tcp:localhost:12368");
-		result = false;
-		results = new ArrayList<>();
+        result = false;
+        results = new ArrayList<>();
         try {
             s1.join("tcp:localhost:12367");
             s2.join("tcp:localhost:12367");
             s1.setListener((tr, msg) -> {
-            		results.add((String)msg.getMessage());
+                results.add((String) msg.getMessage());
             });
             System.out.println(s1.getKeys());
             s2.send(new StringKey("key"), "ABC");
             Thread.sleep(500);
             assertEquals(results.size(), 0);
             EventExecutor.sched("key", 10, () -> {
-            		f = s1.addKeyAsync(new StringKey("key"));
-            		f.whenComplete((ret, ex) -> {
-            			result = ret;
-            			System.out.println(s1.getKeys());
-				});
+                f = s1.addKeyAsync(new StringKey("key"));
+                f.whenComplete((ret, ex) -> {
+                    result = ret;
+                    System.out.println(s1.getKeys());
+                });
             });
             Thread.sleep(500);
             s2.send(new StringKey("key"), "ABC");
@@ -71,24 +72,23 @@ class TestSuzakuKeyAsync {
             assertTrue(result);
             assertEquals(results.size(), 1);
             assertEquals(results.get(0), "ABC");
-        }
-        finally {
+        } finally {
             s1.close();
             s2.close();
         }
     }
-    
+
     @Test
     public void removeKeyInTimer() throws Exception {
         Suzaku<Destination, StringKey> s1 = new Suzaku<>("tcp:localhost:12367");
         Suzaku<Destination, StringKey> s2 = new Suzaku<>("tcp:localhost:12368");
-		result = false;
-		results = new ArrayList<>();
+        result = false;
+        results = new ArrayList<>();
         try {
             s1.join("tcp:localhost:12367");
             s2.join("tcp:localhost:12367");
             s1.setListener((tr, msg) -> {
-            		results.add((String)msg.getMessage());
+                results.add((String) msg.getMessage());
             });
             s1.addKey(new StringKey("key"));
             System.out.println(s1.getKeys());
@@ -96,11 +96,11 @@ class TestSuzakuKeyAsync {
             Thread.sleep(500);
             assertEquals(results.size(), 1);
             EventExecutor.sched("keyremover", 10, () -> {
-            		f = s1.removeKeyAsync(new StringKey("key"));
-            		f.whenComplete((ret, ex) -> {
-            			result = ret;
-            			System.out.println(s1.getKeys());
-				});
+                f = s1.removeKeyAsync(new StringKey("key"));
+                f.whenComplete((ret, ex) -> {
+                    result = ret;
+                    System.out.println(s1.getKeys());
+                });
             });
             Thread.sleep(500);
             s2.send(new StringKey("key"), "ABC");
@@ -108,8 +108,7 @@ class TestSuzakuKeyAsync {
             assertTrue(result);
             assertEquals(results.size(), 1);
             assertEquals(results.get(0), "ABC");
-        }
-        finally {
+        } finally {
             s1.close();
             s2.close();
         }
