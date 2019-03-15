1b5e4de8dcd9a3c3389b38681bce959e22a37cff
teranisi <teranisi@gohome.org>
2019-03-15 18:34:40 +0900

    TestJoin.java: New test.

---
+++ piax-gtrans-suzaku/src/test/java/org/piax/gtrans/ov/suzaku/TestJoin.java
@@ -0,0 +1,16 @@
+package org.piax.gtrans.ov.suzaku;
+
+import org.junit.jupiter.api.Test;
+
+public class TestJoin {
+    String host = "localhost";
+    int port_base = 12367;
+    @Test
+    public void ManyJoinTest() throws Exception {
+        int nodes = 1000;
+        String seed = "tcp:" + host + ":" + port_base;
+        for (int i = 0; i < nodes; i++) {
+            new Suzaku<>("id:*:tcp:" + host + ":" + (port_base + i)).join(seed);
+        }
+    }
+}
